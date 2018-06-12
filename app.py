6b69f385e9c4a83b4192ca605030348fb6db2c61
Stephen Adams <tsadams@gmail.com>
2018-06-12 16:18:22 -0400

    Add ability to check upload status
     Creates an upload/id endpoint so client or users can see status of
    the uploaded package.
    
    - recived
    - validating
    - accepted
    - rejected

--- app.py
+++ app.py
@@ -14,6 +14,9 @@ listen_port = os.getenv('LISTEN_PORT', 8888)
 # to replace this with MQ when it's in place.
 file_dict = {}
 
+# TODO: create an actual persistent store for id status
+status = {}
+
 # all storage below is local for testing. need to decide on a real object store
 # - s3 for permanent, PVC for quarantine zone?
 
@@ -69,11 +72,25 @@ class UploadHandler(tornado.web.RequestHandler):
                 print('Service name does not exist: ' + service)
                 self.set_status(415, 'Unknown Service')
                 self.finish()
+            status[hash_value] = {'upload_status': 'recieved'}
+            self.add_header('Status-Endpoint', '/api/v1/upload/status?id=' + hash_value)
             self.set_status(202, 'Accepted')
             # once MQ is decided on, the service_notify function will go here
 
     def options(self):
         self.add_header('Allow', 'GET, POST, HEAD, OPTIONS')
+
+
+class UploadStatus(tornado.web.RequestHandler):
+    # endpoint for getting information on an individual upload.
+    # Was upload rejected or eccepted by service
+
+    def get(self):
+        upload_id = self.get_argument('id')
+        if upload_id == []:
+            self.set_status(400)
+            return self.finish("Invalid ID")
+        self.write(status[upload_id])
 
 
 class TmpFileHandler(tornado.web.RequestHandler):
@@ -82,30 +99,36 @@ class TmpFileHandler(tornado.web.RequestHandler):
     # remove it. If approved, it moves to a more permanent location with a
     # new URI
 
+    def __init__(self):
+
+        self.hash_value = self.request.uri.split('/')[4]
+
     def get(self):
         buf_size = 4096
-        with open(file_dict[self.request.uri.split('/')[4]], 'r') as f:
+        with open(file_dict[self.hash_value], 'r') as f:
             while True:
                 data = f.read(buf_size)
                 if not data:
                     self.set_status(404)
                     break
+                status[self.hash_value]['upload_status'] = 'validating'
                 self.set_status(200, 'OK')
                 self.write(data)
         self.finish()
 
     def put(self):
-        new_path = "/tmp/new_dir/" + file_dict[self.request.uri.split('/')[4]].split('/')[-1]
-        os.rename(file_dict[self.request.uri.split('/')[4]], new_path)
-        hash_value = uuid.uuid4().hex
-        file_dict[hash_value] = new_path
+        new_path = "/tmp/new_dir/" + file_dict[self.hash_value].split('/')[-1]
+        os.rename(file_dict[self.hash_value], new_path)
+        file_dict[self.hash_value] = new_path
+        status[self.hash_value]['upload_status'] = 'accepted'
         self.set_status(204, 'No Content')
-        self.add_header('Package-URI', "/v1/store/" + hash_value)
+        self.add_header('Package-URI', "/v1/store/" + self.hash_value)
 
     def delete(self):
+        status[self.hash_value]['upload_status'] = 'rejected'
         self.set_status(202, 'Accepted')
-        os.remove(file_dict[self.request.uri.split('/')[4]])
-        file_dict.pop(self.request.uri.split('/')[4], none)
+        os.remove(file_dict[self.hash_value])
+        file_dict.pop(self.hash_value, none)
 
 
 class StaticFileHandler(tornado.web.RequestHandler):
@@ -126,6 +149,7 @@ endpoints = [
     (r"/", RootHandler),
     (r"/api/v1/version", VersionHandler),
     (r"/api/v1/upload", UploadHandler),
+    (r"/api/v1/upload/status", UploadStatus),
     (r"/api/v1/tmpstore/\w{32}", TmpFileHandler),
     (r"/api/v1/store/\w{32}", StaticFileHandler)
 ]
