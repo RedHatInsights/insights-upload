ed15af4ac240473a7047ae182dd4f7e774726570
Stephen Adams <tsadams@gmail.com>
2018-07-16 16:40:35 -0400

    WIP: Local Dev Environment Changes

---
+++ docker/consumer/add-topics.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sudo docker-compose exec kafka \
+kafka-topics --create --topic advisor --partitions 1 --replication-factor 1 --if-not-exists --zookeeper zookeeper:32181
+
+sudo docker-compose exec kafka \
+kafka-topics --create --topic testareno --partitions 1 --replication-factor 1 --if-not-exists --zookeeper zookeeper:32181
+
+sudo docker-compose exec kafka \
+kafka-topics --create --topic uploadvalidation --partitions 1 --replication-factor 1 --if-not-exists --zookeeper zookeeper:32181
+
+sudo docker-compose exec kafka \
+kafka-topics --create --topic available --partitions 1 --replication-factor 1 --if-not-exists --zookeeper zookeeper:32181
