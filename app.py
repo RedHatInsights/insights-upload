906b353df13da017c41de8e28a73c655f0f85784
Stephen Adams <tsadams@gmail.com>
2018-07-17 15:01:31 -0400

    Fix bug with root

--- app.py
+++ app.py
@@ -237,7 +237,7 @@ class VersionHandler(tornado.web.RequestHandler):
 
 
 endpoints = [
-    (r"/api/v1/", RootHandler),
+    (r"/", RootHandler),
     (r"/api/v1/version", VersionHandler),
     (r"/api/v1/upload", UploadHandler),
 ]
