6b0c8f4a7085be54853bbd3772e6c49511e7c5ec
Stephen Adams <tsadams@gmail.com>
2018-09-17 16:08:01 -0400

    Remove references to topic creation
    With  changes to the MQ library, we no longer need to create topics
    ahead of time. They are created automatically upon receiving a message.
    Removed references to topic creation and scripts in place solely for
    that.

--- docker/startup.ps1
+++
@@ -1,6 +0,0 @@
-docker-compose up --build -d
-Start-Sleep -s 5
-docker-compose exec kafka kafka-topics --create --topic advisor --partitions 1 --replication-factor 1 --if-not-exists --zookeeper zookeeper:32181
-docker-compose exec kafka kafka-topics --create --topic testareno --partitions 1 --replication-factor 1 --if-not-exists --zookeeper zookeeper:32181
-docker-compose exec kafka kafka-topics --create --topic uploadvalidation --partitions 1 --replication-factor 1 --if-not-exists --zookeeper zookeeper:32181
-docker-compose exec kafka kafka-topics --create --topic available --partitions 1 --replication-factor 1 --if-not-exists --zookeeper zookeeper:32181
