9de0c7126950a6bb3cff22998e3086b9f010f5cf
Stephen Adams <tsadams@gmail.com>
2019-01-08 12:07:03 -0500

    Update tests for legacy service map

--- tests/test_app.py
+++ tests/test_app.py
@@ -59,7 +59,7 @@ class TestContentRegex(TestCase):
                 self.assertIsNone(search(app.content_regex, mime_type))
 
     def test_supports_legacy(self):
-        self.assertEqual("advisor", app.get_service("application/x-gzip"))
+        self.assertEqual("advisor", app.get_service("application/x-gzip; charset=binary"))
         self.assertEqual("fab", app.get_service("application/vnd.redhat.fab.service+tgz"))
 
 
