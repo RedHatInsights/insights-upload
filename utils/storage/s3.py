ee17936a0e629ca183474c22da2d14ef3344fe26
Stephen Adams <tsadams@gmail.com>
2019-03-06 11:00:56 -0500

    Increase URL expiry for permanent bucket

--- utils/storage/s3.py
+++ utils/storage/s3.py
@@ -38,7 +38,7 @@ def copy(src, dest, uuid):
     s3.delete_object(Bucket=src, Key=uuid)
     url = s3.generate_presigned_url('get_object',
                                     Params={'Bucket': dest,
-                                            'Key': uuid})
+                                            'Key': uuid}, ExpiresIn=86400)
     return url
 
 
