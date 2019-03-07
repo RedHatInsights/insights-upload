c2f1819f26f826f9713ddaa9b4de3958f6acfe4f
Stephen Adams <tsadams@gmail.com>
2019-03-07 11:14:43 -0500

    Multihost support
    Handle post and response changes regarding multihost posts

--- app.py
+++ app.py
@@ -257,12 +257,12 @@ def post_to_inventory(identity, payload_id, values):
     post = values['metadata']
     post['account'] = values['account']
     try:
-        response = requests.post(INVENTORY_URL, json=post, headers=headers)
-        if response.status_code != 200 and response.status_code != 201:
+        response = requests.post(INVENTORY_URL, json=[post], headers=headers)
+        if response.json()['data'][0]['status'] != 200 and response.json()['data'][0]['status'] != 201:
             error = response.json()['data'][0].get('detail')
             logger.error('Failed to post to inventory: ' + error, extra={"payload_id": payload_id})
         else:
-            inv_id = response.json()['data'][0].get('id')
+            inv_id = response.json()['data'][0]['host']['id']
             logger.info('Payload [%s] posted to inventory. ID [%s]', payload_id, inv_id, extra={"payload_id": payload_id,
                                                                                                 "id": inv_id})
         return response.status_code
