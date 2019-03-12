4ab0334ba4a9f35e8056fe62bd03c56ef07395ec
Stephen Adams <tsadams@gmail.com>
2019-03-12 16:29:48 -0400

    Use aioboto3

--- utils/storage/s3.py
+++ utils/storage/s3.py
@@ -1,6 +1,7 @@
 import os
 import asyncio
-import aiobotocore
+import aioboto3
+import boto3
 
 from utils import mnm
 
@@ -18,32 +19,34 @@ QUARANTINE = os.getenv("S3_QUARANTINE", "insights-upload-quarantine")
 PERM = os.getenv("S3_PERM", "insights-upload-perm-test")
 REJECT = os.getenv("S3_REJECT", "insights-upload-rejected")
 
+s3block = boto3.client("s3",
+                       endpoint_url=S3_ENDPOINT_URL,
+                       aws_access_key_id=AWS_ACCESS_KEY_ID,
+                       aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
 
-session = aiobotocore.get_session(loop=IOLoop.current())
-
-async def get_client():
-    return await session.create_client(
+def get_client():
+    return aioboto3.client(
         "s3",
         endpoint_url=S3_ENDPOINT_URL,
         aws_access_key_id=AWS_ACCESS_KEY_ID,
-        aws_secret_access_key=AWS_SECRET_ACCESS_KEY
+        aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
     )
 
 
 class Client(object):
 
     async def __aenter__(self):
-        self.client = await get_client()
+        self.client = get_client()
         return self.client
 
     async def __aexit__(self, exc_type, exc, tb):
-        self.client.close()
+        await self.client.close()
 
 @prom_time(mnm.uploads_s3_write_seconds)
 async def write(data, dest, uuid):
     async with Client() as s3:
         await s3.upload_file(data, dest, uuid)
-        url = await s3.generate_presigned_url(
+        url = s3.generate_presigned_url(
             "get_object", Params={"Bucket": dest,
                                   "Key": uuid}, ExpiresIn=86400
         )
@@ -56,19 +59,18 @@ async def copy(src, dest, uuid):
     async with Client() as s3:
         await s3.copy(copy_src, dest, uuid)
         await s3.delete_object(Bucket=src, Key=uuid)
-        url = await s3.generate_presigned_url(
+        url = s3.generate_presigned_url(
             "get_object", Params={"Bucket": dest,
                                   "Key": uuid}, ExpiresIn=86400
         )
     return url
 
 
-@prom_time(mnm.uploads_s3_ls_seconds)
-async def ls(src, uuid):
+@mnm.uploads_s3_ls_seconds.time()
+def ls(src, uuid):
     try:
-        async with Client() as s3:
-            result = await s3.head_object(Bucket=src, Key=uuid)
-            return result
+        result = s3block.head_object(Bucket=src, Key=uuid)
+        return result
     except ClientError:
         return {"ResponseMetadata": {"HTTPStatusCode": 404}}
 
