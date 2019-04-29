34e478a87ce9236880b569a9ebd56c3c35f843c0
Jesse Jaggars <jjaggars@redhat.com>
2019-04-29 13:00:40 -0500

    moving logging to its own module
    Signed-off-by: Jesse Jaggars <jjaggars@redhat.com>

--- app.py
+++ app.py
@@ -5,9 +5,7 @@ import logging
 import os
 import re
 import base64
-import sys
 import uuid
-import watchtower
 
 from concurrent.futures import ThreadPoolExecutor
 from importlib import import_module
@@ -21,10 +19,11 @@ from kafkahelpers import ReconnectingClient
 
 from aiokafka import AIOKafkaConsumer, AIOKafkaProducer
 from kafka.errors import KafkaError
-from utils import mnm, config
-from logstash_formatter import LogstashFormatterV1
+from utils import mnm, config, log
 from prometheus_async.aio import time as prom_time
-from boto3.session import Session
+
+log.setup_logging()
+logger = logging.getLogger('upload-service')
 
 
 def get_extra(account="unknown", request_id="unknown"):
@@ -33,54 +32,6 @@ def get_extra(account="unknown", request_id="unknown"):
         "request_id": request_id,
     }
 
-
-container = str(uuid.uuid4())
-
-
-class ContextFilter(logging.Filter):
-
-    def filter(self, record):
-        record.container = container
-        return True
-
-
-# Logging
-LOGLEVEL = os.getenv("LOGLEVEL", "INFO")
-if any("KUBERNETES" in k for k in os.environ):
-    handler = logging.StreamHandler(sys.stdout)
-    handler.setFormatter(LogstashFormatterV1())
-    logging.root.setLevel(LOGLEVEL)
-    logging.root.addHandler(handler)
-else:
-    logging.basicConfig(
-        level=LOGLEVEL,
-        format="%(threadName)s %(levelname)s %(name)s - %(message)s"
-    )
-
-logger = logging.getLogger('upload-service')
-other_loggers = [logging.getLogger(n) for n in (
-    'tornado.general',
-    'tornado.application',
-    'kafkahelpers',
-)]
-for l in other_loggers:
-    l.setLevel('ERROR')
-
-for l in (logger, *other_loggers):
-    l.addFilter(ContextFilter())
-
-NAMESPACE = config.get_namespace()
-
-if (config.CW_AWS_ACCESS_KEY_ID and config.CW_AWS_SECRET_ACCESS_KEY):
-    CW_SESSION = Session(aws_access_key_id=config.CW_AWS_ACCESS_KEY_ID,
-                         aws_secret_access_key=config.CW_AWS_SECRET_ACCESS_KEY,
-                         region_name=config.CW_AWS_REGION_NAME)
-    cw_handler = watchtower.CloudWatchLogHandler(boto3_session=CW_SESSION,
-                                                 log_group="platform",
-                                                 stream_name=NAMESPACE)
-    cw_handler.setFormatter(LogstashFormatterV1())
-    for l in (logger, *other_loggers):
-        l.addHandler(cw_handler)
 
 if not config.DEVMODE:
     VALID_TOPICS = config.get_valid_topics()
@@ -286,7 +237,7 @@ class NoAccessLog(tornado.web.RequestHandler):
     """
 
     def _log(self):
-        if LOGLEVEL == "DEBUG":
+        if log.LOGLEVEL == "DEBUG":
             super()._log()
         else:
             pass
