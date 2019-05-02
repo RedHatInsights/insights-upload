5baadc0e93f2a5e1e1c9fb2df3bcf78d60c98f9c
Stephen Adams <tsadams@gmail.com>
2019-05-02 12:41:43 -0400

    Set up a metric to track upload field names
    This is a metric to try to understand how many uploads we have coming in
    using the old `upload` field name. Once this hits 0 over time, we can
    remove the ability to post with this field name

--- utils/mnm.py
+++ utils/mnm.py
@@ -25,6 +25,7 @@ uploads_inventory_post_failure = Counter('uploads_inventory_post_failure', 'The 
 uploads_inventory_post_success = Counter('uploads_inventory_post_success', 'The total number of uploads that succeeded inventory post')
 uploads_produced_to_topic = Counter('uploads_produced_to_queue', 'Total number of messages pushed to the produce_queue for given topic.', ['topic'])
 uploads_popped_to_topic = Counter('uploads_popped_to_queue', 'Total number of messages popped from the produce_queue for given topic.', ['topic'])
+uploads_file_field = Counter('uploads_file_field', 'Total number of payloads recieved using which form field', ['field'])
 
 # Prometheus Summaries
 uploads_write_tarfile = Summary('uploads_write_tarfile_seconds', 'Total seconds it takes to write the tarfile upon upload')
