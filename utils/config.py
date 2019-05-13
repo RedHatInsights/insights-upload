3b158ea28bde717292d5c9db0fe2f1b7aa003065
Stephen Adams <tsadams@gmail.com>
2019-05-13 13:50:37 -0400

    Move variables into config
    Move valid_topics into config
    Call get_commit_date directly
    Fix valid_topics list

--- utils/config.py
+++ utils/config.py
@@ -108,11 +108,12 @@ def get_valid_topics():
         return VALID_TOPICS
     except Exception:
         logger.exception("Unable to open topics.json. Using default topics")
-        VALID_TOPICS = [{"TOPIC NAME": "platform.upload.validation"},
-                        {"TOPIC NAME": "platform.upload.testareno"},
-                        {"TOPIC NAME": "platform.upload.hccm"},
-                        {"TOPIC NAME": "platform.upload.compliance"},
-                        {"TOPIC NAME": "platform.upload.qpc"}]
+        VALID_TOPICS = ["advisor",
+                        "validation",
+                        "testareno",
+                        "hccm",
+                        "compliance",
+                        "qpc"]
         return VALID_TOPICS
 
 
@@ -130,3 +131,6 @@ def get_commit_date(commit_id):
         date = "unknown"
 
     return date
+
+
+VALID_TOPICS = get_valid_topics()
