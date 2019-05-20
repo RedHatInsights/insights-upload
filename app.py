28a64e83917fc89ad1732ffd6072fdbb9eaa0f28
Stephen Adams <tsadams@gmail.com>
2019-05-20 15:59:59 -0400

    Fix merge conflicts

--- app.py
+++ app.py
@@ -405,7 +405,7 @@ class UploadHandler(tornado.web.RequestHandler):
             logger.exception("Unsupported Media Type: [%s] - Request-ID [%s]", self.payload_data['content_type'], self.request_id, extra=extra)
             return self.error(415, 'Unsupported Media Type', **extra)
         if serv_dict["service"] not in VALID_TOPICS:
-            logger.error("Unsupported MIME type: [%s] - Request-ID [%s]", self.payload_data['content_type'], self.payload_id, extra=extra)
+            logger.error("Unsupported MIME type: [%s] - Request-ID [%s]", self.payload_data['content_type'], self.request_id, extra=extra)
             return self.error(415, 'Unsupported MIME type', **extra)
 
     def get(self):
