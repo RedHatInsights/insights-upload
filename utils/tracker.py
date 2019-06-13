ac4a8d128b05437c091b6edd8f6af0dd0588bae1
Stephen Adams <tsadams@gmail.com>
2019-06-13 14:55:12 -0400

    Fix double-quotes in payload_status

--- utils/tracker.py
+++ utils/tracker.py
@@ -12,9 +12,9 @@ def payload_tracker(request_id, account, status, status_msg):
     payload_msg = {"topic": config.TRACKER_TOPIC}
 
     payload_status = {
-        'service': config.APP_NAME,
-        'status': status,
-        'status_msg': status_msg,
+        "service": config.APP_NAME,
+        "status": status,
+        "status_msg": status_msg,
         "account": account,
         "request_id": request_id,
         "payload_id": request_id,
